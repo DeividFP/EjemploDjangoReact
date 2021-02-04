95404f2dea0f73240927ab572639c265c9e2a2b0
David Farelas <farelasdavid.98@gmail.com>
2021-02-03 22:38:58 -0600

    Prueba 1

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+import { func } from 'prop-types'
+import React from 'react'
+import Form from './Form'
+import Leads from './Leads'
+
+export default function Dashboard () {
+    return (
+        <>
+            <Form />
+            <Leads />
+        </>
+    )
+}
